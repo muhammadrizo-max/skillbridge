e3c6f31aed2d21cd8066b5253008a5979d1aedd1
VisionCoreGroup <muhammadrizoxudoberdiyev834@gmail.com>
2026-07-01 15:42:29 -0300

    Update postcss.config.mjs

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,2 +1,8 @@
-const config = { plugins: { tailwindcss: {}, autoprefixer: {} } }
+/** @type {import('postcss-load-config').Config} */
+const config = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
 export default config
